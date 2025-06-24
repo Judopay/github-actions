240d75ec19222ce77b6386fe8ce4b857052122c3
David Pollock <david.pollock@judopay.com>
2025-06-24 15:03:16 +1000

    Fix pod dir detection

--- actions/pod-install/install.sh
+++ actions/pod-install/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- IFS=','; for FILE in "$PODFILE_LOCKS"; do
+IFS=','; for FILE in "$PODFILE_LOCKS"; do
    echo "Installing pods for $FILE"
    cd "$(dirname "$FILE")"
    pod install || exit 1
--- actions/pod-install/scan.sh
+++ actions/pod-install/scan.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PODFILE_LOCKS=""
-POD_DIRS=""
+PODFILE_LOCKS=()
+POD_DIRS=()
 while IFS= read -r -d '' FILE; do
   echo "Found Podfile.lock: $FILE"
-  PODFILE_LOCKS="${PODFILE_LOCKS}${PODFILE_LOCKS:+,}$FILE"
-  POD_DIRS="${POD_DIRS}${POD_DIRS:+,}$(dirname $FILE)/Pods"
+  PODFILE_LOCKS+=("$FILE")
+  POD_DIRS+=("$(dirname "$FILE")/Pods")
 done < <(find . -name Podfile.lock -not -path './Carthage/*' -type f -print0)
 
-echo "PODFILE_LOCKS=$PODFILE_LOCKS" >> $GITHUB_OUTPUT
-echo "POD_DIRS=$POD_DIRS" >> $GITHUB_OUTPUT
+printf -v var '%s,' "${PODFILE_LOCKS[@]}" && echo "PODFILE_LOCKS=${var%,}" >> $GITHUB_OUTPUT
+printf -v var '%s,' "${POD_DIRS[@]}" && echo "POD_DIRS=${var%,}" >> $GITHUB_OUTPUT
