5802c170e7e7d00b6b4eb4f98404530efc908a9d
David Pollock <david.pollock@judopay.com>
2025-06-24 14:08:37 +1000

    Minor fixes

--- actions/pod-install/scan.sh
+++ actions/pod-install/scan.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-$PODFILE_LOCKS=""
-$POD_DIRS=""
+PODFILE_LOCKS=""
+POD_DIRS=""
 while IFS= read -r -d '' FILE; do
   echo "Found Podfile.lock: $FILE"
   PODFILE_LOCKS+="$FILE,"
   POD_DIRS+="$(dirname \"$FILE\")/Pods,"
 done < <(find . -name Podfile.lock -not -path './Carthage/*' -type f -print0)
 
-echo "PODFILE_LOCKS=blah" >> $GITHUB_OUTPUT
-echo "POD_DIRS=blah" >> $GITHUB_OUTPUT
+echo "PODFILE_LOCKS=$PODFILE_LOCKS" >> $GITHUB_OUTPUT
+echo "POD_DIRS=$POD_DIRS" >> $GITHUB_OUTPUT
