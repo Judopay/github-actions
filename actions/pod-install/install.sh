a22d04c5f39cb8daf07ae977156529b04fed1c59
David Pollock <david.pollock@judopay.com>
2025-06-24 15:39:03 +1000

    Fix for pod install action

--- actions/pod-install/install.sh
+++ actions/pod-install/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-IFS=','; for FILE in "$PODFILE_LOCKS"; do
-   echo "Installing pods for $FILE"
-   cd "$(dirname "$FILE")"
-   pod install || exit 1
-done
+while IFS= read -r -d '' FILE; do
+  echo "Installing pods for $FILE"
+  cd "$(dirname "$FILE")"
+  pod install || exit 1
+done < <(find . -name Podfile.lock -not -path './Carthage/*' -type f -print0)
