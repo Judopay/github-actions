1a291fa5c91f376b62f8b34a8b9ac1f9fe72fd40
David Pollock <david.pollock@judopay.com>
2025-06-24 14:28:09 +1000

    Fix pod dir detection

--- actions/pod-install/scan.sh
+++ actions/pod-install/scan.sh
@@ -5,9 +5,7 @@ POD_DIRS=""
 while IFS= read -r -d '' FILE; do
   echo "Found Podfile.lock: $FILE"
   PODFILE_LOCKS="${PODFILE_LOCKS}${PODFILE_LOCKS:+,}$FILE"
-  POD_DIRS="${POD_DIRS}${POD_DIRS:+,}$(dirname \"$FILE\")/Pods"
-  echo "$PODFILE_LOCKS"
-  echo "$POD_DIRS"
+  POD_DIRS="${POD_DIRS}${POD_DIRS:+,}$(dirname $FILE)/Pods"
 done < <(find . -name Podfile.lock -not -path './Carthage/*' -type f -print0)
 
 echo "PODFILE_LOCKS=$PODFILE_LOCKS" >> $GITHUB_OUTPUT
