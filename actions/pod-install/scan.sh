c497431eff034e5d3243fddad879d9dad32cf4a2
David Pollock <david.pollock@judopay.com>
2025-06-24 15:07:09 +1000

    Debugging

--- actions/pod-install/scan.sh
+++ actions/pod-install/scan.sh
@@ -10,3 +10,5 @@ done < <(find . -name Podfile.lock -not -path './Carthage/*' -type f -print0)
 
 printf -v var '%s,' "${PODFILE_LOCKS[@]}" && echo "PODFILE_LOCKS=${var%,}" >> $GITHUB_OUTPUT
 printf -v var '%s,' "${POD_DIRS[@]}" && echo "POD_DIRS=${var%,}" >> $GITHUB_OUTPUT
+
+cat $GITHUB_OUTPUT
