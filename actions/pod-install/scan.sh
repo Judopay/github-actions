e9aa5ec594899dbd05b1de976612d85d4c28e133
David Pollock <david.pollock@judopay.com>
2025-06-24 14:04:09 +1000

    Fix for pod install caching

--- actions/pod-install/scan.sh
+++ actions/pod-install/scan.sh
@@ -6,7 +6,7 @@ while IFS= read -r -d '' FILE; do
   echo "Found Podfile.lock: $FILE"
   PODFILE_LOCKS+="$FILE,"
   POD_DIRS+="$(dirname \"$FILE\")/Pods,"
-done < <(find $HOME -name Podfile.lock -type f -print0)
+done < <(find . -name Podfile.lock -not -path './Carthage/*' -type f -print0)
 
 echo "PODFILE_LOCKS=blah" >> $GITHUB_OUTPUT
 echo "POD_DIRS=blah" >> $GITHUB_OUTPUT
