0d1cb5a735c0ccd21d464b5051470d67414a355d
David Pollock <david.pollock@judopay.com>
2025-06-24 15:42:35 +1000

    Fix for pod install action

--- actions/pod-install/install.sh
+++ actions/pod-install/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 while IFS= read -r -d '' FILE; do
+  CURR_DIR=$(pwd)
   echo "Installing pods for $FILE"
   cd "$(dirname "$FILE")"
   pod install || exit 1
+  cd "$CURR_DIR"
 done < <(find . -name Podfile.lock -not -path './Carthage/*' -type f -print0)
