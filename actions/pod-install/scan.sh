52230eb6e845a107f87aee699012538b7cff54e8
David Pollock <david.pollock@judopay.com>
2025-06-24 14:17:13 +1000

    Fix for podfile scanner

--- actions/pod-install/scan.sh
+++ actions/pod-install/scan.sh
@@ -4,8 +4,8 @@ PODFILE_LOCKS=""
 POD_DIRS=""
 while IFS= read -r -d '' FILE; do
   echo "Found Podfile.lock: $FILE"
-  PODFILE_LOCKS+="$FILE,"
-  POD_DIRS+="$(dirname \"$FILE\")/Pods,"
+  PODFILE_LOCKS="${PODFILE_LOCKS}${PODFILE_LOCKS:+,}$FILE"
+  POD_DIRS="${POD_DIRS}${POD_DIRS:+,}$(dirname \"$FILE\")/Pods"
 done < <(find . -name Podfile.lock -not -path './Carthage/*' -type f -print0)
 
 echo "PODFILE_LOCKS=$PODFILE_LOCKS" >> $GITHUB_OUTPUT
