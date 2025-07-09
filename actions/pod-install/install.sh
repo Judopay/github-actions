c917833d497f63314697a7a60605b9a1dacb4ca3
David Pollock <david.pollock@judopay.com>
2025-07-10 09:14:02 +1000

    Use bundle exec when running pod install

--- actions/pod-install/install.sh
+++ actions/pod-install/install.sh
@@ -4,6 +4,6 @@ while IFS= read -r -d '' FILE; do
   CURR_DIR=$(pwd)
   echo "Installing pods for $FILE"
   cd "$(dirname "$FILE")"
-  pod install || exit 1
+  bundle exec pod install || exit 1
   cd "$CURR_DIR"
 done < <(find . -name Podfile.lock -not -path './Carthage/*' -type f -print0)
