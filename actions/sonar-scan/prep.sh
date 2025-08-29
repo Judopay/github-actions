bf9b72d659d2bbad5a2b9fd9e93b72a598703d00
David Pollock <david.pollock@judopay.com>
2025-08-29 14:36:33 +1000

    Improve sonar scan test detection

--- actions/sonar-scan/prep.sh
+++ actions/sonar-scan/prep.sh
@@ -48,7 +48,7 @@ if [ -d "$HOME/.gradle/caches" ]; then
     PARAMS+=(-Dsonar.java.libraries="$HOME/.gradle/caches/**/*.jar")
 fi
 
-if TEST_LOCATIONS="$(find $SONAR_SOURCES -not -path '**/node_modules/*' -wholename '*/src/test' | tr '\n' ',')" && [ -n "${TEST_LOCATIONS}" ]; then
+if TEST_LOCATIONS="$(find $SONAR_SOURCES \( -not -path '**/node_modules/*' -a \( -wholename '*/src/test' -o -wholename '**/__tests__' -o -wholename '**/__mocks__' \) \) | tr '\n' ',')" && [ -n "${TEST_LOCATIONS}" ]; then
     echo "Found test locations: $TEST_LOCATIONS. Adding to SonarQube parameters."
     PARAMS+=(-Dsonar.tests="$TEST_LOCATIONS")
     TEST_EXCLUSIONS=$(echo "$TEST_LOCATIONS" | sed "s~$SONAR_SOURCES/~~g" | sed 's/,/\/**\/*,/g')
